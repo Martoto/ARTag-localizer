a3546918618f36803102e7ac61eea17f38f8a4f7
Martoto <baronwalrus@gmail.com>
2022-11-24 19:30:18 +0000

    send to  localhost and esp - ric

--- server.py
+++ server.py
@@ -2,13 +2,15 @@ import socket
 import threading
 import queue
 
-HOSTPORT = ("localhost", 5070)
+HOSTPORTS = [("localhost", 5070), ("192.168.101.3", 5070)]
+
 
 def send_pose(pose):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = (str(pose) + "\n").encode()
     #print("sending data: ", data)
-    sock.sendto(data, HOSTPORT)
+    for hostport in HOSTPORTS:
+        sock.sendto(data, hostport)
 
 def run_server():
     return None
